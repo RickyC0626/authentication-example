63b3cc5601a3695c5c717d3ec5f32eafe8c3c472
Ricky Chon <rickychon99@gmail.com>
2022-11-18 17:42:40 -0500

    Add company logo on top of forms

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -8,7 +8,12 @@ export default function Home() {
         min-w-full min-h-screen bg-gradient-to-t from-blue-700/80 via-indigo-800/90 to-violet-900/90
         grid place-items-center
       ">
-        <LoginForm />
+        <div className="grid gap-8">
+          <h1 className="text-3xl text-white font-bold text-center sm:text-4xl">
+            Company Logo
+          </h1>
+          <LoginForm />
+        </div>
       </main>
     </div>
   );
