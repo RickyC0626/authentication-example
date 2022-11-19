36793e2bd8c2ceffd4092bc08d241bc125b6f249
Ricky Chon <rickychon99@gmail.com>
2022-11-18 22:03:09 -0500

    Add icons to input fields

--- ui/components/LoginForm.tsx
+++ ui/components/LoginForm.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 import {
   StyledForm,
   StyledFormButton,
@@ -20,26 +22,32 @@ export default function LoginForm({ onClickSignUp }: {
       <StyledFormDivider />
       <StyledFormFieldSection>
         <StyledFormFieldLabel htmlFor="login_email_field">Email</StyledFormFieldLabel>
-        <StyledFormFieldInput
-          type="email"
-          id="login_email_field"
-          name="login_email_field"
-          placeholder="john.doe@email.com"
-          required
-        />
+        <div className="flex">
+          <MdOutlineEmail className="absolute place-self-center w-6 h-6 translate-x-3 text-white/80" />
+          <StyledFormFieldInput
+            type="email"
+            id="login_email_field"
+            name="login_email_field"
+            placeholder="john.doe@email.com"
+            required
+          />
+        </div>
       </StyledFormFieldSection>
       <StyledFormFieldSection>
         <StyledFormFieldLabel htmlFor="login_password_field">Password</StyledFormFieldLabel>
-        <StyledFormFieldInput
-          type="password"
-          id="login_password_field"
-          name="login_password_field"
-          placeholder="********"
-          required
-          minLength={8}
-          maxLength={40}
-          title="8-40 characters"
-        />
+        <div className="flex">
+          <RiLockPasswordLine className="absolute place-self-center w-6 h-6 translate-x-3 text-white/80" />
+          <StyledFormFieldInput
+            type="password"
+            id="login_password_field"
+            name="login_password_field"
+            placeholder="********"
+            required
+            minLength={8}
+            maxLength={40}
+            title="8-40 characters"
+          />
+        </div>
       </StyledFormFieldSection>
       <StyledFormButtonSection>
         <StyledFormSubmitButton
--- ui/components/RegistrationForm.tsx
+++ ui/components/RegistrationForm.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 import {
   StyledForm,
   StyledFormDivider,
@@ -16,41 +18,50 @@ export default function RegistrationForm() {
       <StyledFormDivider />
       <StyledFormFieldSection>
         <StyledFormFieldLabel htmlFor="signup_email_field">Email</StyledFormFieldLabel>
-        <StyledFormFieldInput
-          type="email"
-          id="signup_email_field"
-          name="signup_email_field"
-          placeholder="john.doe@email.com"
-          required
-        />
+        <div className="flex">
+          <MdOutlineEmail className="absolute place-self-center w-6 h-6 translate-x-3 text-white/80" />
+          <StyledFormFieldInput
+            type="email"
+            id="signup_email_field"
+            name="signup_email_field"
+            placeholder="john.doe@email.com"
+            required
+          />
+        </div>
       </StyledFormFieldSection>
       <StyledFormFieldSection>
         <StyledFormFieldLabel htmlFor="signup_password_field">
           Password <span className="text-sm font-normal">(minimum 8 characters)</span>
         </StyledFormFieldLabel>
-        <StyledFormFieldInput
-          type="password"
-          id="signup_password_field"
-          name="signup_password_field"
-          placeholder="********"
-          required
-          minLength={8}
-          maxLength={40}
-          title="8-40 characters"
-        />
+        <div className="flex">
+          <RiLockPasswordLine className="absolute place-self-center w-6 h-6 translate-x-3 text-white/80" />
+          <StyledFormFieldInput
+            type="password"
+            id="signup_password_field"
+            name="signup_password_field"
+            placeholder="********"
+            required
+            minLength={8}
+            maxLength={40}
+            title="8-40 characters"
+          />
+        </div>
       </StyledFormFieldSection>
       <StyledFormFieldSection>
         <StyledFormFieldLabel htmlFor="signup_confirm_password_field">Confirm Password</StyledFormFieldLabel>
-        <StyledFormFieldInput
-          type="password"
-          id="signup_confirm_password_field"
-          name="signup_confirm_password_field"
-          placeholder="********"
-          required
-          minLength={8}
-          maxLength={40}
-          title="8-40 characters"
-        />
+        <div className="flex">
+          <RiLockPasswordLine className="absolute place-self-center w-6 h-6 translate-x-3 text-white/80" />
+          <StyledFormFieldInput
+            type="password"
+            id="signup_confirm_password_field"
+            name="signup_confirm_password_field"
+            placeholder="********"
+            required
+            minLength={8}
+            maxLength={40}
+            title="8-40 characters"
+          />
+        </div>
       </StyledFormFieldSection>
       <StyledFormSubmitButton
         type="submit"
