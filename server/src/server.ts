1c38d38cfa975307e431cde62ca32fea991b3657
Ricky Chon <rickychon99@gmail.com>
2022-11-20 17:05:38 -0500

    Extract server from main

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import authRouter from "./routes/auth";
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms", {
+    skip: (req, res) => process.env.NODE_ENV === "test",
+  })
+);
+
+server.get("/health", (req: express.Request, res: express.Response) => {
+  res.status(200).send("Healthy!");
+});
+
+server.use("/", authRouter);
+
+export default server;
