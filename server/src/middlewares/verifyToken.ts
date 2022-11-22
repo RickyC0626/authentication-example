db4227eff9c8b3e6524b942d2a13061dc9a6e688
Ricky Chon <rickychon99@gmail.com>
2022-11-22 14:24:31 -0500

    Secure tokens in http-only cookies

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -1,21 +1,18 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { e } from "../../types/express";
 
 export const verifyToken = (
-  req: e.Request,
+  req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const auth = req.headers["authorization"];
-  const token = auth && auth.split(" ")[1];
+  const accessToken = req.cookies?.access_token;
 
-  if(!token) return res.sendStatus(401);
+  if(!accessToken) return res.sendStatus(401);
 
-  jwt.verify(token, (process.env.JWT_SECRET as string), (err: any, user: any) => {
+  jwt.verify(accessToken, (process.env.JWT_SECRET as string), (err: any, decoded: any) => {
     if(err) return res.sendStatus(403);
 
-    req.user = user;
     next();
   });
 };
